89c2299532aff3c442178c91f81e2131f2e879e1
Francesc Puig <francescpuig7@gmail.com>
2019-09-14 20:44:46 +0200

    Add regex dictnames

--- spiders/dictnames.py
+++ spiders/dictnames.py
@@ -2,3 +2,19 @@ DICTNAMES = [
     'marquez', 'vinales', 'Marc Marquez', 'maverick', 'dovi', 'dovizioso', 'dovicioso', 'rossi', 'rins', 'miller',
     'jack'
 ]
+
+REGEX_PILOTS = [
+    'Marc Marquez*', 'Marquez*', 'marc marquez*', 'marquez*', 'marques*', 'Andrea Dovizioso*', 'Dovi*', 'dovi*',
+    'Zarco*', 'zarco*', 'Petrux*', 'Petrucci*', 'Petruci*',
+    'petrux*', 'petrucci*', 'petruci*', 'Maverick Vi[a-z]ales*', 'Maverick*', 'Viñales*', 'Viñales*', 'Maveric*',
+    'maverick vi[a-z]ales*', 'maverick*', 'maveric*', 'Iannone*', 'Ianone*', 'iannone*', 'ianone*', 'Mir*', 'mir*',
+    'Aleix Espargaro*', 'Alex Rins*', 'Rins*', 'alex rins*', 'rins*', 'Miller*', 'miller*', 'Jack Miller*',
+    'jack miller*', 'Pol Espargar*', 'pol espargar*', 'Valentino Rossi*', 'Rossi*', 'Rosi*', 'rossi*', 'rosi*',
+    'valentino rossi*', 'valentino rosi*', 'pirro*', 'Pirro*', 'Hafizh Syahrin*', 'Syahrin*', 'syahrin*', 'shyarin*',
+    'Bagnaia*', 'bagnaia*', 'Oliveira*', 'oliveira*', 'lorenzo*', 'Lorenzo*'
+]
+
+REGEX_POSITIONS = [
+    'pole*', 'Pole*', 'vr*', 'vuelta r[a-m]pida*', '1*', '2*', '3*'
+]
+
